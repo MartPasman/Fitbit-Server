c3869de3fe3f81fda985c8402f7d91035696f279
svenordelman <sveno98@live.nl>
2017-05-15 14:07:56 +0200

    user added

---
+++ model/model_account.js
@@ -0,0 +1,38 @@
+/**
+ * Created by sveno on 15-5-2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var fitbitSchema = new Schema({
+    userid: {type: String, required:true},
+    accesToken: {type: String, required:true},
+    refreshtoken: {type: String, required:true}
+});
+
+module.exports = mongoose.model('fitbit', fitbitSchema);
+
+var goalSchema = new Schema({
+    id: {type: Number, required:true},
+    start: {type: Date, required:true},
+    end: {type: Date, required:true},
+    progress: {type: Number, required:true, default: 0},
+    percentage: {type: Number, required:true, default: 0}
+
+});
+
+module.exports = mongoose.model('goal', goalSchema);
+
+var userSchema = new Schema({
+    id: {type: Number, required: true, unique: true},
+    password: {type: String, required: true},
+    salt: {type: String, required: true},
+    fitbit: {type: fitbitSchema, required: false},
+    goals: [goalSchema],
+    email: {type: String, required: true},
+    active: {type: bool, required: true, default: true},
+    handicap: {type: Number, required: false, default: 1},
+    type: {type: Number, required: true, default: 1}
+});
+
+module.exports = mongoose.model('user', userSchema);
---
+++ model/model_competition.js
@@ -0,0 +1,26 @@
+/**
+ * Created by sveno on 15-5-2017.
+ */
+/**
+ * Created by sveno on 15-5-2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var resultSchema = new Schema({
+    userid: {type: String, required:true},
+    score: {type: Number, required:true},
+    goalAchieved: {type: bool, required:true},
+});
+
+module.exports = mongoose.model('result', resultSchema);
+
+var competitionSchema = new Schema({
+    id: {type: Number, required:true},
+    goal: {type: Number, required:true},
+    start: {type: Date, required:true},
+    end: {type: Date, required:true},
+    results: [resultSchema]
+});
+
+module.exports = mongoose.model('competition', competitionSchema);
