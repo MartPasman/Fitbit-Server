5c3eee29810f08cb9271d8bf8f6037958fc30df5
svenordelman <sveno98@live.nl>
2017-05-15 15:41:45 +0200

    Login

--- routes/route_accounts.js
+++ routes/route_accounts.js
@@ -6,9 +6,66 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require('mongoose');
-var Account = require('../model/account');
 var User = require('../model/model_user');
 var shortid = require('shortid');
 var bcrypt = require('bcrypt-nodejs');
-
 var jwt = require('jsonwebtoken');
+
+var logResponse = require('../index').logResponse;
+
+
+app.post('/login', function (req, res) {
+
+    if (req.body.id === undefined || req.body.password === undefined) {
+        logResponse(400, 'id or password is not supplied');
+        return res.status(400).send({error: 'id or password is not supplied'});
+    }
+
+    console.log('\tID:\t' + req.body.id+ '\n\tpassword:\t*****');
+
+    // Find the user
+    User.findOne({id: req.body.id}, {_id: 0, __v: 0}, function (err, user) {
+
+        // Check to see whether an error occurred
+        if (err) {
+            logResponse(500, err.message);
+            return res.status(500).send({error: err.message});
+        }
+
+        // Check to see whether a user was found
+        if (!user) {
+            logResponse(400, 'Invalid credentials');
+            return res.status(400).send({error: "Invalid credentials"});
+        }
+
+        try {
+            // Check to see whether the given password matches the password of the user
+            bcrypt.compare(req.body.password, user.password, function (err, success) {
+                if (err) {
+                    logResponse(500, err.message);
+                    return res.status(500).send({error: err.message});
+                }
+
+                if (!success) {
+                    logResponse(400, 'Invalid credentials');
+                    return res.status(400).send({error: "Invalid credentials"});
+                }
+
+                // remove sensitive data
+                user.password = undefined;
+
+                // sign json web token (expires in 12 hours)
+                var token = jwt.sign(user, req.app.get('private-key'), {expiresIn: (60 * 60 * 12)});
+
+                logResponse(201, 'Token created and in body');
+                res.status(201).send({
+                    succes: token
+                });
+            });
+        } catch (err) {
+            // if the bcrypt fails
+            logResponse(500, err.message);
+            return res.status(500).send({error: err.message});
+        }
+    });
+});
