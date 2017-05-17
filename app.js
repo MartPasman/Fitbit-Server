94ca5f5b99f080f6363bc83d7d86d19db5ad3c9e
Jeroen Smienk <jeroensmienk@icloud.com>
2017-05-17 14:28:22 +0200

    fix

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //set account routes
-var accountsRoutes = require('./routes/route_accounts');
-app.use('/accounts', accountsRoutes);
+// var accountsRoutes = require('./routes/route_accounts');
+// app.use('/accounts', accountsRoutes);
 
 //set competition routes
-var competitionRoutes = require('./routes/route_competitions');
-app.use('/competitions', competitionRoutes);
+// var competitionRoutes = require('./routes/route_competitions');
+// app.use('/competitions', competitionRoutes);
 
 //set user routes
-var userRoutes = require('./routes/route_users');
-app.use('/accounts/users', userRoutes);
+// var userRoutes = require('./routes/route_users');
+// app.use('/accounts/users', userRoutes);
 
 //sends a 400 (bad request if the user send a invalid request)
 app.use(function (error, req, res, next) {
