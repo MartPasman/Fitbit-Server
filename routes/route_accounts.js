084c772d3a7bcc602ae85995678058e7a2fb15e8
svenordelman <sveno98@live.nl>
2017-05-23 12:16:40 +0200

    rest fix

--- routes/route_accounts.js
+++ routes/route_accounts.js
@@ -177,28 +177,29 @@ app.post('/login', function (req, res) {
  * all requests below this function will automatically go through this one first!
  * If your page doesn't need to be requested by admin, put it above this function
  */
-// app.use('/', function (req, res, next) {
-//
-//     console.log('\tAuthentication required...');
-//     console.log(req.app.get('private-key'));
-//     jwt.verify(req.get("Authorization"), req.app.get('private-key'), function (err, decoded) {
-//         if (err) {
-//             logResponse(401, err.message);
-//             return res.status(401).send({error: "User is not logged in."});
-//         }
-//
-//         // Save user for future purposes
-//         res.user = decoded._doc;
-//         if (res.user.type !== 3) {
-//             logResponse(403, "Not authorized to make this request");
-//             return res.status(403).send({error: "Not authorized to make this request"});
-//         }
-//
-//         console.log('\tpassed');
-//
-//         next();
-//     });
-// });
+app.use('/', function (req, res, next) {
+
+    console.log('\tAuthentication required...');
+    console.log(req.app.get('private-key'));
+    jwt.verify(req.get("Authorization"), req.app.get('private-key'), function (err, decoded) {
+        if (err) {
+            logResponse(401, err.message);
+            return res.status(401).send({error: "User is not logged in."});
+        }
+
+        // Save user for future purposes
+        res.user = decoded._doc;
+        if (res.user.type !== 3) {
+            logResponse(403, "Not authorized to make this request");
+            return res.status(403).send({error: "Not authorized to make this request"});
+         }
+
+         console.log('\tpassed');
+
+         next();
+     });
+
+});
 
 /**
  * Make new account
