0a53cfbf870a41f48d4b2ed8dc88c688566effd3
Jeroen Smienk <jeroensmienk@icloud.com>
2017-06-12 19:48:57 +0200

    added new comp fields added default values

--- model/model_competition.js
+++ model/model_competition.js
@@ -3,19 +3,24 @@ var Schema = mongoose.Schema;
 
 var resultSchema = new Schema({
     userid: {type: Number, required: true},
-    score: {type: Number, required: true},
-    goalAchieved: {type: Boolean, required: true}
+    score: {type: Number, required: true, default: 0},
+    goalAchieved: {type: Boolean, required: true, default: false}
 });
 
 module.exports = mongoose.model('result', resultSchema);
 
 var competitionSchema = new Schema({
     id: {type: Number, required: true, unique: true},
-    goal: {type: Number, required: true},
-    defaultGoal: {type: Number, required: true},
-    defaultLength: {type: Number, required: true},
-    start: {type: Date, required: true},
-    end: {type: Date, required: true},
+    start: {type: Date, required: true, default: new Date()},
+    end: {type: Date, required: true, default: new Date().setDate(new Date().getDate() + 6)},
+    goal: {type: Number, required: true, default: 3000},
+    defaultGoal: {type: Number, required: true, default: 3000},
+    sharedGoal: {type: Number, required: true, default: 50000},
+    sharedScore: {type: Number, required: true, default: 0},
+    defaultSharedGoal: {type: Number, required: true, default: 50000},
+    sharedGoalProgress: {type: Number, required: true, default: 0},
+    sharedGoalAchieved: {type: Boolean, required: true, default: false},
+    defaultLength: {type: Number, required: true, default: 7},
     results: [resultSchema]
 });
 
