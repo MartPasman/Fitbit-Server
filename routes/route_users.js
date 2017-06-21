a87ebe69494737c5141a979b4a23eb7572fefb16
Jeroen Smienk <jeroensmienk@icloud.com>
2017-06-21 11:27:37 +0200

    .

--- routes/route_users.js
+++ routes/route_users.js
@@ -583,7 +583,7 @@ app.put('/:id', function (req, res) {
     // if the active state was provided
     if (!(req.body.active === undefined || req.body.active === '')) {
         // if it is a boolean
-        if (req.body.active === true || req.body.active === false) {
+        if (Boolean(req.body.active) instanceof Boolean) {
             json.active = req.body.active;
         }
     }
